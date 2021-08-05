87829c2e15ee812e9903f3a802a98c7844bf51f8
Ted Verdonkschot <tedverdonkschot@outlook.com>
2021-08-05 13:43:45 -0700

    Add functions to allow number and decimal input

--- app.js
+++ app.js
@@ -6,11 +6,27 @@ const calculator = {
     operator: null,
 };
 
+// Get value from number keys
+function inputNumber(number) {
+    const { displayValue } = calculator;
+    // If display value is zero, overwrite it with number, else append number
+    calculator.displayValue = displayValue === '0' ? number : displayValue + number;
+}
+// Append decimal
+function inputDecimal(decimal) {
+    // Only add a decimal if there already isn't one 
+    if (!calculator.displayValue.includes(decimal)) {
+        calculator.displayValue += decimal;
+    }
+}
+
 // Get calculator display and update its value
 function updateDisplay() {
-    const display = document.querySelector('.calculator-screen');
+    const display = document.querySelector('.calculator-display');
     display.value = calculator.displayValue;
 };
+
+updateDisplay();
 
 // Get calculator keys and add event listeners to each
 const keys = document.querySelector('.calculator-keys');
@@ -19,7 +35,7 @@ keys.addEventListener('click', (e) => {
     // Exit function if the target is not a button
     if (!target.matches('button')) {
         return;
-    };
+    }
 
     if (target.classList.contains('operator')) {
         console.log('operator', target.value);
@@ -27,7 +43,8 @@ keys.addEventListener('click', (e) => {
     }
 
     if (target.classList.contains('decimal')) {
-        console.log('decimal', target.value);
+        inputDecimal(target.value);
+        updateDisplay();
         return;
     }
 
@@ -36,6 +53,7 @@ keys.addEventListener('click', (e) => {
         return;
     }
 
-    console.log('number', target.value);
+    inputNumber(target.value);
+    updateDisplay();
 });
 
