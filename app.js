8f887d3cd334932175bb67a8b815558085707b5d
Ted Verdonkschot <tedverdonkschot@outlook.com>
2021-08-05 13:19:14 -0700

    Remove operation functions, add calculator object, display and key functions

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
-// Basic math operations
-function add(a, b) { return a + b};
-function subtract(a, b) {return a - b};
-function multiply(a, b) {return a * b};
-function divide(a, b) {return a / b};
+// Track values used in each expression
+const calculator = {
+    displayValue: '0',
+    firstValue: null,
+    waitingOnSecondValue: false,
+    operator: null,
+};
 
-// Operation
-function operate(a, b, operator) {
-    if (operator == "+") {
-        add(a, b);
-    } else if (operator == "-") {
-        subtract(a, b);
-    } else if (operator == "*") {
-        multiply(a, b);
-    } else if (operator == "/") {
-        divide(a, b);
-    } else {
-        console.log("Not a valid operator.")
+// Get calculator display and update its value
+function updateDisplay() {
+    const display = document.querySelector('.calculator-screen');
+    display.value = calculator.displayValue;
+};
+
+// Get calculator keys and add event listeners to each
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (e) => {
+    const { target } = e;
+    // Exit function if the target is not a button
+    if (!target.matches('button')) {
+        return;
+    };
+
+    if (target.classList.contains('operator')) {
+        console.log('operator', target.value);
+        return;
     }
-}
+
+    if (target.classList.contains('decimal')) {
+        console.log('decimal', target.value);
+        return;
+    }
+
+    if (target.classList.contains('all-clear')) {
+        console.log('clear', target.value);
+        return;
+    }
+
+    console.log('number', target.value);
+});
+
